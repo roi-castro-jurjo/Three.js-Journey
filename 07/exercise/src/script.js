b32fb7e8f6c2e55180da888638f73b3853996e8e
Roi Castro <roi.castro.jurjo@rai.usc.es>
2024-01-22 19:07:42 +0100

    Ended Lesson 07

--- 07/exercise/src/script.js
+++ 07/exercise/src/script.js
@@ -22,9 +22,43 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+})
+
+window.addEventListener('dblclick', () => {
+
+    const fullScreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullScreenElement){
+        if (canvas.requestFullscreen()){
+            canvas.requestFullscreen()
+        } else if (canvas.webkitRequestFullscreen()) {
+            canvas.webkitRequestFullscreen()
+        }
+    } else {
+        if (document.exitFullscreen()){
+            document.exitFullscreen()
+        } else if(document.webkitExitFullscreen()){
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
+
 
 /**
  * Camera
@@ -33,6 +67,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -45,6 +81,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
