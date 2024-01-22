0226d9e32244a678c95076a3372f11811e3b391c
Roi Castro <roi.castro.jurjo@rai.usc.es>
2024-01-22 22:46:05 +0100

    Started lesson 09

--- 09/exercise/src/script.js
+++ 09/exercise/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
 
 /**
  * Base
@@ -10,36 +11,17 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-
-// const positionArray = new Float32Array([
-//     0,0,0,
-//     0,1,0,
-//     0,0,1
-// ])
-
-// const positionAttribute = new THREE.BufferAttribute(positionArray, 3)
-
-// const geometry = new THREE.BufferGeometry()
-// geometry.setAttribute('position', positionAttribute)
-
-const geometry = new THREE.BufferGeometry()
-
-const count = 1000
-const positionArray = new Float32Array(count * 3 * 3)
-for(let i = 0; i < count * 3 * 3; i++){
-    positionArray[i] = (Math.random() - 0.5) 
-}
-
-const positionAttribute = new THREE.BufferAttribute(positionArray, 3)
-geometry.setAttribute('position', positionAttribute)
-
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -60,23 +42,32 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
